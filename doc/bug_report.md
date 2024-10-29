0bd08539efc14ed99bab99ce1c39b6f54fca9d1b
aakash aman <aakash.aman@student.sl.on.ca>
2024-10-28 23:43:55 -0400

    bug#1 found

--- doc/bug_report.md
+++ doc/bug_report.md
@@ -6,4 +6,18 @@
     * show code fragment contaiing the bug
     * show corrected code
         
-2. Repeat for all bugs
+2. Bug #1 the main page shows welcome Null insted of name 
+    * Access login page >> Type in anything >> Click Login
+    * Model and User parameters are not passed into the Main function.
+    * ```@GetMapping("/main")
+            public String getMain() {
+                logger.info(">>>> We are at the main endpoint.");
+                return "main";
+                }```
+    * ```@GetMapping("/main")
+            public String getMain(Model model, User user){
+                logger.info(">>>> We are at the main endpoint.");
+                model.addAttribute("user", user.getUserId());
+                return "main";
+                }```
+
--- src/main/java/comp31/lab4/controllers/MainController.java
+++ src/main/java/comp31/lab4/controllers/MainController.java
@@ -1,5 +1,8 @@
 package comp31.lab4.controllers;
 
+import org.slf4j.Logger;                // import logger from slf4j
+import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+    
+    Logger logger = LoggerFactory.getLogger(MainController.class);
 
     public MainController() {
         super();
@@ -17,18 +22,21 @@ public class MainController {
 
     @GetMapping("/")
     public String getRoot(Model model) {
+        logger.info("<<<<<<<<< at get root >>>>>>>>>");
         model.addAttribute("user", new User());
         return "login";
     }
 
     @PostMapping("/login")
     public String postLogin(User user) {
+        logger.info("<<<<<<<<<<< at /login >>>>>>>>>>>");
         String next = "redirect:/main?user=" + user.getUserId();
         return next;
     }
 
     @GetMapping("/main")
     public String getMain() {
+        logger.info("<<<<<<<<<<at /main get method >>>>>>>>>>");
         return "main";
     }
 
